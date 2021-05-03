cea7832bf1962305feace6cdef5edf92313517ae
19011785-SiewYongYing <19011785@myrp.edu.sg>
2021-05-03 11:07:04 +0800

    SectionD - Making changes to Radio Button Gender

--- app/src/main/java/sg/edu/rp/c346/id19011785/demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19011785/demosimpleclick/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnClick;
     EditText etInput;
     ToggleButton tbtn;
+    RadioGroup rgGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.textViewDisplay);
         etInput = findViewById(R.id.editTextInput);
         tbtn = findViewById(R.id.toggleButtonEnabled);
+        rgGender = findViewById(R.id.radioGroupGender)
 
         btnClick.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +48,25 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code for the action
+                String stringResponse = etInput.getText().toString();
+                int checkedRadioId = rgGender.getCheckedRadioButtonId();
+                if(checkedRadioId == R.id.radioButtonGenderMale){
+                    // Write the code when male selected
+
+                }
+                else{
+                    // Write the code when female selected
+
+                }
+                tvDisplay.setText(stringResponse);
+            }
+        });
+
 
     }
 }
